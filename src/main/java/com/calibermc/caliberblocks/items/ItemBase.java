2af2b737c45707ffc316d8c731fe76fc5678b2c9
Josh <josh@luttmer.us>
2021-11-09 10:49:17 -0700

    Initial commit

---
+++ src/main/java/com/calibermc/caliberblocks/items/ItemBase.java
@@ -0,0 +1,12 @@
+package com.calibermc.caliberblocks.items;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.item;
+
+public class ItemBase extends Item {
+
+    public ItemBase() {
+        super(new Item.Properties().group);
+    }
+}
